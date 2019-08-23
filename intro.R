97a6444ded7cb11d3742d3b2467666bf381e2ab3
Ken Hatogai <hatogaik@exeggutor.cs.uchicago.edu>
2019-08-23 12:46:09 -0500

    am

--- intro.R
+++ intro.R
@@ -45,7 +45,7 @@ days <- c(3,5,7)
 inf_days <- inf_data[pts,days]
 
 inf_days_all_pt <- inf_data[,pts]
-inf_days_all_pt <- inf_data[,days]
+inf_days_all_days <- inf_data[,days]
 
 max(car_data$Speed) - min(car_data$Speed)
 max_speed <- max(car_data$Speed)
@@ -53,8 +53,95 @@ car_data[car_data$Speed == max_speed, ]
 
 car_data[car_data$Speed == max_speed,]
 
+## Let's review
+days2 <- c(3,5,7)
+days3 <- c(1:5)
 
 
+## assigning values to variables
+ x <- 2
+# data <- read.csv(...)
+# y <- 3
+# x <- y
+ 
+# subsetting data
+day_3 <- inf_data$V3 
+day_3[1:3]
+day_3[1]
+length(day_3)
+num_pt <- length(day_3)
+day_3[num_pt]
+
+# last three?
+day_3[58:60]
+day_3[c(58,59,60)]
+length(day_3)-2
+
+begin <- length(day_3)-2
+day_3[begin:num_pt] 
+begin <- length(day_3)
+
+## index by name
+car_data[,c("Color", "Speed")]
+car_color_speed <- car_data[,c("Color", "Speed")]
+inf_data[, c("V1","V3")]
+
+## logical indexing (boolean incexing, TRUE/FALSE indexing)
+22 == 11*2 #TURE
+day_3 == 1
+day_3[day_3 == 1]
+day_3_ones <- day_3[day_3 == 1]
+length(day_3_ones)
+day_3_no_ones <- day_3[day_3 !=1]
+length(day_3_no_ones)
+max(day_3_ones)
+min(day_3_ones)
+max(day_3_no_ones)
+min(day_3_no_ones)
+
+is_one <- day_3 == 1
+day_5 <- inf_data$V5
+day_5[is_one]
+day_5_day_3_one <- day_5[is_one]
+max(day_5_day_3_one)
+inf_data[day_3 == 1, ]
+
+inf_data$V5[inf_data$V5 > 0]
+inf_data$V5[inf_data$V5 > 2]
+
+pts <- 1:10
+pts < 0
+pts[pts < 0]
+
+#red cars are faster thatn blue cars
+
+color_speed <- car_data[,c("Color", "Speed")]
+car_data$Color == "Blue"
+color_blue <- car_data$Color == "Blue"
+car_data$Speed[color_blue]
+speed_blue <- car_data$Speed[color_blue]
+mean(speed_blue)
+
+car_data$Color == "Red"
+color_red <- car_data$Color == "Red"
+car_data$Speed[color_red]
+speed_red <- car_data$Speed[color_red]
+mean(speed_red)
+
+mean(speed_blue) > mean(speed_red)
 
 
+## answer
+car_speed <- car_data$Speed
+car_color <- car_data$Color
+mean(car_speed[car_color == "Red"])
+mean(car_speed[car_color == "Blue"])
 
+write.csv(car_data, "./data/car_data.csv",
+          row.names = FALSE
+          )
+## help menu: single ? and double ??
+?read.csv
+?write.csv
+?plot
+??plot
